1c218c133fffaf4fe84213275ffab0910bdb750e
Артем Орлов <Artem Orlov>
2025-04-09 17:04:18 +0300

    Initial Commit

---
+++ FinanceManager/FinanceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerApp.swift
+//  FinanceManager
+//
+//  Created by Artem on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
